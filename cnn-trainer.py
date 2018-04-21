6ff3b73ef6fdbb6008a2ea1f69c9cb4f8e177e80
Ulysses Yu <ulysses_yu@yahoo.com>
2018-04-21 17:08:52 -0500

    one-hot conversion added

--- cnn-trainer.py
+++ cnn-trainer.py
@@ -13,6 +13,9 @@ img_size = 32
 # Number of channels in each image, 3 channels: Red, Green, Blue.
 num_channels = 3
 
+# Number of classes
+num_class = 10
+
 def unpickle(file):
     """
     Loads the data into a dictionary
@@ -42,27 +45,33 @@ def convert_images(raw):
 def load_data(filename):
     """
     Load a pickled data-file from the CIFAR-10 data-set
-    and return the converted images (see above) and the class-number
-    for each image.
+    and return the converted images and the class-number for each image.
     """
 
     # Load the pickled data-file.
     data = unpickle(filename)
-
     # Get the raw images.
     raw_images = data[b'data']
-
     # Get the class-numbers for each image. Convert to numpy-array.
     cls = np.array(data[b'labels'])
-
     # Convert the images.
     images = convert_images(raw_images)
 
     return images, cls
 
+def one_hot_encoder(array, n):
+    """
+    Takes an array with the shape (length,) and returns an array of shape (length,n)
+    N is the number that will be one-hot encoded into
+    """
+    length = array.shape[0]
+    one_hot = np.zeros((length, n+1))
+    one_hot[np.arange(length), array] = 1
+    
+    return one_hot
+
 images1, cls1 = load_data('cifar-10-batches-py/data_batch_1')
 print(images1.shape)
 print(cls1.shape)
-# print(dict1.shape)
-# print(dict1.count)
-# print(dict1.keys())
+
+Y_vec = one_hot_encoder(cls1, num_class)
