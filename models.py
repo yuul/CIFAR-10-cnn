7ab3b3df1d5531f488dc6153a2aaf1e43175cd65
Ulysses Yu <ulysses_yu@yahoo.com>
2018-04-30 14:00:17 -0500

    Just made an update prior to BatchNorm

--- models.py
+++ models.py
@@ -188,3 +188,31 @@ def simplest_model():
     # Trains the model with ADAM
     model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
     return model
+
+def batchnorm_model():
+    
+    model = Sequential()
+    
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(img_size, img_size, num_channels)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(BatchNormalization())
+    model.add(Dense(num_class, activation='softmax'))
+
+    # Trains the model with ADAM
+    model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+    return model
